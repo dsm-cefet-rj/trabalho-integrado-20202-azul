3180014ef71addb58f9a22aee5f528774c0dcc75
dd2t <matheusddsm@gmail.com>
2021-03-13 21:31:24 -0300

    Added to App.js a state for the loaded character

--- godfather/src/main-app/app.js
+++ godfather/src/main-app/app.js
@@ -25,18 +25,80 @@ class App extends React.Component {
     // eslint-disable-next-line
     constructor (props) {
         super(props);
+
+        // Binding this
+        this.characterSetting = this.characterSetting.bind(this)
+        
+        // Creating a state
+        this.state = {
+            character: {
+                picture: {},
+                name: 'NAP',
+                status: [0, 0, 0, 0],
+                equipament: [{}, {}, {}, {}],
+                inventory: [{}, {}, {}, {}, {}, {}, {}, {}],
+                activeMission: {}
+            }
+        }
+
+    }
+
+    componentWillMount () {
+        this.characterSetting()
     }
 
     render() {
-        const testPlayer = {
+        return(
+            <Router>
+                {/* Logo / Menu*/}
+                <header>
+                    <section id="logo">
+                        <Logo />
+                    </section>
+                    <section id="menu">
+                        <Menu />
+                    </section>
+                </header>
+
+                {/* Card */}
+                <main>
+                    <section id="card">
+                        <Switch>
+                            {/* Your card component goes here */}
+                            <Route exact path="/"><Home /></Route>
+                            <Route path="/character"><Character player={this.state.character}/></Route>
+                            {/* <Route path="/missions"><Missions missions={testMisions} /></Route> */}
+                            <Route path="/duels"><Duels /></Route>
+                            {/* <Route path="/market"><Market /></Route> */}
+                            <Route path="/gold-shop"><GoldShop /></Route>
+                        </Switch>
+                    </section>
+                </main>
+
+                {/* Footer */}
+                <footer>
+                    <section id="footer">
+                        <Footer />
+                    </section>
+                </footer>
+            </Router>
+        );
+    }
+
+    characterSetting() {
+
+        // Requesting character data
+        const tempCharacter = {
             picture: testPlayerImage,
             name: 'Don Luffino',
             status: [21, 42, 10, 23],
             equipament: [{}, {}, {}, {}],
-            inventory: [{}, {}, {}, {}, {}, {}, {}, {}]
+            inventory: [{}, {}, {}, {}, {}, {}, {}, {}],
+            activeMission: {}
         }
 
-        const testMisions = {
+        // Requesting mission data
+        const tempMisions = {
             missionsArray: [
                 {
                     name: 'Sicilian immigrant',
@@ -61,41 +123,11 @@ class App extends React.Component {
                 }
             ]
         }
-        return(
-            <Router>
-                {/* Logo / Menu*/}
-                <header>
-                    <section id="logo">
-                        <Logo />
-                    </section>
-                    <section id="menu">
-                        <Menu />
-                    </section>
-                </header>
 
-                {/* Card */}
-                <main>
-                    <section id="card">
-                        <Switch>
-                            {/* Your card component goes here */}
-                            <Route exact path="/"><Home /></Route>
-                            <Route path="/character"><Character player={testPlayer}/></Route>
-                            <Route path="/missions"><Missions missions={testMisions} /></Route>
-                            <Route path="/duels"><Duels /></Route>
-                            {/* <Route path="/market"><Market /></Route> */}
-                            <Route path="/gold-shop"><GoldShop /></Route>
-                        </Switch>
-                    </section>
-                </main>
-
-                {/* Footer */}
-                <footer>
-                    <section id="footer">
-                        <Footer />
-                    </section>
-                </footer>
-            </Router>
-        );
+        // Inserting data in state
+        this.setState({
+            character: tempCharacter
+        })
     }
 }
 
