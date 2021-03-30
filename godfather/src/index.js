6d58a1fa27e91c2362729609a1edf591739aef62
dd2t <matheusddsm@gmail.com>
2021-03-30 16:29:03 -0300

    Appo and (store)index file names changed To app.js and store.js

--- godfather/src/index.js
+++ godfather/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './main-app/appo';
+import App from './main-app/app';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
-import store from './store/index'
+import store from './store/store'
 
 ReactDOM.render(
   <React.StrictMode>
--- godfather/src/main-app/app.js
+++ godfather/src/main-app/app.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import Logo from '../header-footer/logo/logo'
 import Menu from '../header-footer/menu/menu'
 import Footer from '../header-footer/footer/footer'
-import { Provider } from 'react-redux'
-import store from '../store'
+
+import { useSelector, useDispatch } from 'react-redux'
 
 import {
     BrowserRouter as Router,
@@ -19,144 +19,124 @@ import Missions from '../card-components/missions/missions'
 import Character from '../card-components/character/character'
 import GoldShop from '../card-components/gold/gold-shop'
 
-// imports to testPlayer
-import testPlayerImage from '../card-components/character/mafia-luffy.jpg'
 
+const App = () => {
 
-class App extends React.Component {
+    const characterSelector = useSelector(state => state)
+    // const dispatch = useDispatch()
 
-    constructor (props) {
-        super(props);
+    const [character, setCharacter] = useState({
+        picture: {},
+        name: '',
+        reputation: 0,
+        status: [0, 0, 0, 0],
+        equipament: [{}, {}, {}, {}],
+        inventory: [{}, {}, {}, {}, {}, {}, {}, {}],
+        activeMission: {}
+    })
 
-        // Binding this
-        this.characterSetting = this.characterSetting.bind(this)
-        
-        // Defining the state
-        this.state = {
-            character: {
-                picture: {},
-                name: 'NAP',
-                reputation: 0,
-                status: [0, 0, 0, 0],
-                equipament: [{}, {}, {}, {}],
-                inventory: [{}, {}, {}, {}, {}, {}, {}, {}],
-                activeMission: {}
+    const [missions, setMissions] = useState({
+        missionArray: [
+            {
+                name: 'Sicilian immigrant',
+                description: 'blabla',
+                xp: 20,
+                cash: 50,
+                time: 3
             },
-
-            missions: {
-                missionArray: [
-                    {
-                        name: '',
-                        description: '',
-                        xp: 0,
-                        cash: 0,
-                        time: 0
-                    }
-                ]
+            {
+                name: 'Fender Ketchup',
+                description: 'blabla',
+                xp: 10,
+                cash: 10,
+                time: 1
             },
-
-            goldOffers: {
-                offerArray: [
-                    {
-                        title: '',
-                        picture: {},
-                        price: 0,
-                        bonus: ''
-                    }
-                ]
+            {
+                name: 'Just business',
+                description: 'blabla',
+                xp: 15,
+                cash: 30,
+                time: 2
             }
-        }
+        ]
+    })
 
-    }
+    const [goldOffers, setGoldOffers] = useState({
+        offerArray: [
+            {
+                title: 'Herança do Don',
+                value: 1200,
+                picture: {},
+                price: 200,
+                bonus: '20%'
+            },
+            {
+                title: 'Dote de casamento',
+                value: 575,
+                picture: {},
+                price: 100,
+                bonus: '15%'
+            },
+            {
+                title: 'Pensão alimentícia',
+                value: 275,
+                picture: {},
+                price: 50,
+                bonus: '10%'
+            }
+        ]
+    })
 
-    componentWillMount () {
-        this.characterSetting()
-    }
+    useEffect(() => {
+        // fetch('/player-data').then(res => res.json()).then(data => {
+        //     dispatch({
+        //         type: 'SET_CHARACTER',
+        //         picture: data.character.picture,
+        //         name: data.character.name,
+        //         status: data.character.status,
+        //         reputation: data.character.reputation,
+        //         equipament: data.character.equipament,
+        //         inventory: data.character.inventory,
+        //         activeMission: data.character.activeMission
+        //     })
+        // })
+        setCharacter(characterSelector)
+        console.log( 'CHARACTER STATE', character)
+    })
+    // useEffect without []
 
-    render() {
-        return(
-            <Provider store={store} >
-                <Router>
-                    {/* Logo / Menu*/}
-                    <header>
-                        <section id="logo">
-                            <Logo />
-                        </section>
-                        <section id="menu">
-                            <Menu />
-                        </section>
-                    </header>
-                    {/* Card */}
-                    <main>
-                        <section id="card">
-                            <Switch>
-                                {/* Your card component goes here */}
-                                <Route exact path="/"><Home /></Route>
-                                <Route path="/character"><Character player={this.state.character}/></Route>
-                                <Route path="/missions"><Missions missions={this.state.missions} /></Route>
-                                <Route path="/duels"><Duels player={this.state.character} /></Route>
-                                {/* <Route path="/market"><Market /></Route> */}
-                                <Route path="/gold-shop"><GoldShop goldOffers={this.state.goldOffers} /></Route>
-                            </Switch>
-                        </section>
-                    </main>
-                    {/* Footer */}
-                    <footer>
-                        <section id="footer">
-                            <Footer />
-                        </section>
-                    </footer>
-                </Router>
-            </Provider>
-        );
-    }
-
-    characterSetting() {
-
-        // Requesting character data
-        const tempCharacter = {
-            picture: testPlayerImage,
-            name: 'Don Luffino',
-            status: [21, 42, 10, 23],
-            reputation: 20,
-            equipament: [{}, {}, {}, {}],
-            inventory: [{}, {}, {}, {}, {}, {}, {}, {}],
-            activeMission: {}
-        }
-
-        // Requesting mission data
-        const tempMisions = {
-            missionArray: [
-                {
-                    name: 'Sicilian immigrant',
-                    description: 'blabla',
-                    xp: 20,
-                    cash: 50,
-                    time: 3
-                },
-                {
-                    name: 'Fender Ketchup',
-                    description: 'blabla',
-                    xp: 10,
-                    cash: 10,
-                    time: 1
-                },
-                {
-                    name: 'Just business',
-                    description: 'blabla',
-                    xp: 15,
-                    cash: 30,
-                    time: 2
-                }
-            ]
-        }
-
-        // Inserting data in state
-        this.setState({
-            character: tempCharacter,
-            missions: tempMisions
-        })
-    }
+    return (
+            <Router>
+                {/* Logo / Menu*/}
+                <header>
+                    <section id="logo">
+                        <Logo />
+                    </section>
+                    <section id="menu">
+                        <Menu />
+                    </section>
+                </header>
+                {/* Card */}
+                <main>
+                    <section id="card">
+                        <Switch>
+                            {/* Your card component goes here */}
+                            <Route exact path="/"><Home /></Route>
+                            <Route path="/character"><Character /></Route>
+                            <Route path="/missions"><Missions missions={missions} /></Route>
+                            <Route path="/duels"><Duels player={character} /></Route>
+                            <Route path="/gold-shop"><GoldShop goldOffers={goldOffers} /></Route>
+                        </Switch>
+                    </section>
+                </main>
+                {/* Footer */}
+                <footer>
+                    <section id="footer">
+                        <Footer />
+                    </section>
+                </footer>
+            </Router>
+    )
 }
 
 export default App;
