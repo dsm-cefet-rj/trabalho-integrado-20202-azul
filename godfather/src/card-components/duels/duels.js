a68521ecaa64b09bf71e602b9a72ac792679817a
Kyougaru <gabrielterock@gmail.com>
2021-04-14 23:22:53 -0300

    duels.js updated

--- godfather/src/card-components/duels/duels.js
+++ godfather/src/card-components/duels/duels.js
@@ -1,4 +1,6 @@
 import './Duels.css';
+import DefaultCharacterPicture from './mafia-luffy.jpg'
+import { useState, useEffect } from 'react'
 
 /**
  * @module duels/duels
@@ -12,42 +14,53 @@ import './Duels.css';
  */
 
 function Duels(props) {
+	const [character, setCharacter] = useState([])
+	
+	useEffect(() => {
+        fetch('/api/character').then(res => res.json()).then(data => {
+            if(!data) {
+                return
+            } else {
+                setCharacter(data.character)
+            }
+        })
+    }, [])
+
     return (
         <>
             <div className="card-title">
                 <h2 className="display-6">Duels</h2>
             </div>
+			<div id="wanted-poster">
+				<h3 id="wanted-title">WANTED</h3>
+				<h4 id="wanted-subtitle">Dead or Alive</h4>
+				<p className="character-name">{character.name}</p>
+				<img id="character-img" src={character.picture} alt="Your character" className="img-fluid" />
 
+				<div id="status-row" className="row">
+					<div className="col">
+						<div id="atk-sts">
+							Atk: <span>{character.status.atk}</span>
+						</div>
+					</div>
+					<div className="col">
+						<div id="res-sts">
+							Res: <span>{character.status.res}</span>
+						</div>
+					</div>
+					<div className="col">
+						<div id="lck-sts">
+							Lck: <span>{character.status.lck}</span>
+						</div>
+					</div>
+					<div className="col">
+						<div id="rsl-sts">
+							Rsl: <span>{character.status.rsl}</span>
+						</div>
+					</div>
+				</div>
+			</div>
             <div className="card-content">
-                <div id="wanted-poster">
-                    <h3 id="wanted-title">WANTED</h3>
-                    <h4 id="wanted-subtitle">Dead or Alive</h4>
-                    <p className="character-name">Don Luffino</p>
-                    <img id="character-img" src={process.env.PUBLIC_URL + '/images/test/cards/duels/mafia-luffy.jpg'} alt="Your character" className="img-fluid" />
-
-                    <div id="status-row" className="row">
-                        <div className="col">
-                            <div id="atk-sts">
-                                Atk: <span>18</span>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div id="res-sts">
-                                Res: <span>18</span>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div id="lck-sts">
-                                Lck: <span>18</span>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div id="rsl-sts">
-                                Rsl: <span>18</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <div id="button-box" className="buttons-row d-flex justify-content-center">
                     <button id="queue-button" className="btn" data-bs-toggle="modal" data-bs-target="#queue-modal">Queue</button>
                 </div>
