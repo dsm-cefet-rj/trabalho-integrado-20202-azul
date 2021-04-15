abe7746228bd59007399d7541e973991ef22dcff
dd2t <matheusddsm@gmail.com>
2021-04-14 21:17:49 -0300

    Mission and gold offer queries

--- backend/routes/api-gold.js
+++ backend/routes/api-gold.js
@@ -1,33 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const GoldOffer = require('../models/goldOffers')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	res.json({
-        offerArray: [
-            {
-                title: 'Herança do Don',
-                value: 1200,
-                picture: {},
-                price: 200,
-                bonus: '20%'
-            },
-            {
-                title: 'Dote de casamento',
-                value: 575,
-                picture: {},
-                price: 100,
-                bonus: '15%'
-            },
-            {
-                title: 'Pensão alimentícia',
-                value: 275,
-                picture: {},
-                price: 50,
-                bonus: '10%'
-            }
-        ]
-    })
+
+    GoldOffer.find((err, offers) => {
+        res.json({ offerArray: offers })
+    }).limit(3)
+    
 });
 
 module.exports = router;
--- backend/routes/api-missions.js
+++ backend/routes/api-missions.js
@@ -1,33 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const Mission = require('../models/missions')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	res.json({
-		missionList: [
-            {
-                name: 'Sicilian immigrant',
-                description: '',
-                xp: 20,
-                cash: 50,
-                time: 3
-            },
-            {
-                name: 'Fender Ketchup',
-                description: '',
-                xp: 10,
-                cash: 10,
-                time: 1
-            },
-            {
-                name: 'Just business',
-                description: '',
-                xp: 15,
-                cash: 30,
-                time: 2
-            }
-        ]
-	})
+
+    Mission.find((err, missions) => {
+        res.json({ missionList: missions })
+    }).limit(3)
+    
 });
 
 module.exports = router;
