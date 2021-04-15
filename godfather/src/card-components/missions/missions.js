c8417827e4a10dac2e1a3454c7be0138956c641c
dd2t <matheusddsm@gmail.com>
2021-04-15 10:53:44 -0300

    Mission modal data fixed

--- godfather/src/card-components/missions/missions.js
+++ godfather/src/card-components/missions/missions.js
@@ -13,6 +13,7 @@ import { useState, useEffect } from 'react'
 
 function Missions () {
     const [missionArray, setMissionArray] = useState([])
+    let modalMission = {}
 
     useEffect(() => {
         fetch('/api/missions').then(res => res.json()).then(data => {
@@ -23,6 +24,14 @@ function Missions () {
         })
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
+    const fillModalMission = (element) => {
+        modalMission = { ...element }
+
+        // Time formart
+        const zeroPad = (num, places) => String(num).padStart(places, '0')
+        modalMission.time = zeroPad(modalMission.time, 2)
+    }
     
     const missionList = () => {
         if (!missionArray) {
@@ -30,48 +39,12 @@ function Missions () {
         }
 
         let array = []
-        let i = 0
         missionArray.forEach(element => {
             array.push(
-				<button className="list-group-item list-group-item-action" data-bs-toggle="modal" data-bs-target={"#mission-details-modal-"+i}>
+				<button className="list-group-item list-group-item-action" data-bs-toggle="modal" data-bs-target="#mission-details-modal" key={element._id} onClick={fillModalMission(element)}>
 					{element.name}
 				</button>
             )
-			array.push(
-				<div className="modal fade" id={"mission-details-modal-"+i} tabIndex="-1">
-					<div className="modal-dialog modal-dialog-centered">
-						<div className="modal-content">
-							<div className="modal-header">
-								<h5 className="modal-title" id="modal-mission-name">{element.name}</h5>
-								<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-							</div>
-							<div className="modal-body">
-								<p id="modal-mission-description">{element.description}</p>
-								<p id="modal-mission-sender" className="text-end">
-									Don Corleone
-								</p>
-								<div id="modal-mission-rewards" className="row">
-									<div className="col">
-										<p>Rewards: </p>
-									</div>
-									<div className="col" id="xp-reward">
-										<span>{element.xp}</span> XP
-									</div>
-									<div className="col" id="money-reward">
-										$ <span>{element.cash}</span>
-									</div>
-								</div>
-							</div>
-							<div className="modal-footer">
-								<p>Time: {element.time}</p>
-								<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-								<button type="button" className="btn btn-success">Combat</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			)
-            i++
         })
 
         return (<div>{array}</div>)
@@ -123,9 +96,9 @@ function Missions () {
                         <div className="col" id="xp-reward">
                             <strong><span>20</span></strong> XP
                         </div>
-                        <div className="col" id="money-reward">
+                        {/* <div className="col" id="money-reward">
                             $ <strong><span>50</span></strong>
-                        </div>
+                        </div> */}
                     </div>
 
                     <div id="ac-actions">
@@ -144,6 +117,42 @@ function Missions () {
                 </div>
             </div>
 			
+            {/* Accepting mission modal */}
+                {/* Modal */}
+                <div className="modal fade" id="mission-details-modal" tabIndex="-1">
+                    <div className="modal-dialog modal-dialog-centered">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="modal-mission-name">{modalMission.name}</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <p id="modal-mission-description">
+                                    {modalMission.description}
+                                </p>
+                                <p id="modal-mission-sender" className="text-end">
+                                    {modalMission.sender}
+                                </p>
+                                <div id="modal-mission-rewards" className="row">
+                                    <div className="col">
+                                        <p>Rewards: </p>
+                                    </div>
+                                    <div className="col" id="xp-reward">
+                                        <span>{modalMission.xp}</span> XP
+                                    </div>
+                                    {/* <div className="col" id="money-reward">
+                                        $ <span>50</span>
+                                    </div> */}
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <p>Time: {modalMission.time}:00</p>
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" className="btn btn-success">Combat</button>
+                            </div>
+                        </div>
+                    </div>
+               </div>
         </>
     );
 }
